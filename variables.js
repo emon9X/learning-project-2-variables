e1636271e3636d5f285e36351cbcebe15f1352cc
unknown <shahariar.emn990@gmail.com>
2023-12-23 12:22:28 +0600

    global scope and block scope

--- variables.js
+++ variables.js
@@ -7,3 +7,23 @@ y = 6; /*here y is not declared explicitly, assigned value and used. As it is us
 z = x + y; /*here z is not declared explicitly, assigned value and used. As it is used, it is declared by javascript automatically*/
 console.log(x, y, z)
 // ------------------------------------------------------------------------
+
+// global scope and block scope 
+
+const name = 'emon';  /*global scope variable*/
+let age = 28; /*global scope variable*/
+var hobby = 'gardening'; /*global scope variable*/
+
+{
+    console.log(name, age, hobby) /*global scope variable can be accessed from a block*/
+    var weight = 72; /*global scope variable. var is not block scoped*/
+    let hair = 'black'; /*block scope variable*/
+    const university = 'ruet'; /*block scope variable*/
+}
+
+console.log(weight); /*gives the value as it is a global scope variable*/
+// console.log(hair); /*gives ReferenceError*/ 
+// console.log(university); /*gives ReferenceError*/
+// -------------------------------------------------------------------------
+
+
