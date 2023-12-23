233def8ce920c817c39221656cdbaf460872159c
unknown <shahariar.emn990@gmail.com>
2023-12-23 12:21:15 +0600

    how to automatically declare a variable

--- variables.js
+++ variables.js
@@ -1,7 +1,9 @@
-function automaticVariableDeclaration() {
-    x = 5; /*here x is not declared explicitly but assigned value*/
-    y = 6; /*here y is not declared explicitly but assigned value*/
-    z = x + y; /*here z is not declared explicitly but assigned value*/
-    console.log(x, y, z) /*here the variables are declared automatically as they are first used*/
-    document.getElementById("auto").innerHTML =  "The vaue of z is: " +z;
-}
+// how to automatically declare a variable 
+// x; /* as it is not declared explicitly by var or let or const keyword, it will give an error(x is not defined)*/
+// y; /* as it is not declared explicitly by var or let or const keyword, it will give an error(y is not defined)*/
+// z; /* as it is not declared explicitly by var or let or const keyword, it will give an error(z is not defined)*/
+x = 5; /*here x is not declared explicitly, assigned value and used. As it is used, it is declared by javascript automatically*/
+y = 6; /*here y is not declared explicitly, assigned value and used. As it is used, it is declared by javascript automatically*/
+z = x + y; /*here z is not declared explicitly, assigned value and used. As it is used, it is declared by javascript automatically*/
+console.log(x, y, z)
+// ------------------------------------------------------------------------
