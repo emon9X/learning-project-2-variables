6560b60e804c36c5681d78050c39f1a4707e8178
unknown <shahariar.emn990@gmail.com>
2023-12-23 12:22:58 +0600

    hoisting, declaring variables, assigning and re-assigning and initialization

--- variables.js
+++ variables.js
@@ -27,3 +27,26 @@ console.log(weight); /*gives the value as it is a global scope variable*/
 // -------------------------------------------------------------------------
 
 
+// hoisting, declaring variables, assigning and re-assigning and initialization 
+
+// block is created for convenience 
+{
+    // hoisted line 
+
+    // console.log(a) /*Uncaught ReferenceError ReferenceError: a is not defined. a is not declared explicitly*/
+
+    // var keyword 
+
+    console.log(b);
+    var b/*b is declared by var keyword. this declaration is hoisted at the hoisted line(35). then variable b is initialized with a value which is undefined. This means var b is as same as var b = undefined*/
+
+    var c = 1;
+    console.log(c);
+    var c = 2; /*can be redeclared and reassigned*/
+    console.log(c)
+
+    // let keyword 
+
+    let d = 'john doe';
+    // let c = 0 /*can't be redeclared */
+}
