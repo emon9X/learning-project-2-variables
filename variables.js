a0f8b70f3b093cd00fac4b28219b15512b2cca21
unknown <shahariar.emn990@gmail.com>
2023-12-23 12:43:31 +0600

    2nd phase hoisting

--- variables.js
+++ variables.js
@@ -38,9 +38,9 @@ console.log(weight); /*gives the value as it is a global scope variable*/
     // var keyword 
 
     console.log(b);
-    var b/*b is declared by var keyword. this declaration is hoisted at the hoisted line(35). then variable b is initialized with a value which is undefined. This means var b is as same as var b = undefined*/
+    var b/*b is declared by var keyword and no value is assigned thus not initialized. this declaration is hoisted at the hoisted line(35). then variable b is initialized with a value which is undefined. This means var b is as same as var b = undefined*/
 
-    var c = 1;
+    var c = 1; /*c is declared and has assigned or initialized value */
     console.log(c);
     var c = 2; /*can be redeclared and reassigned*/
     console.log(c)
@@ -48,5 +48,15 @@ console.log(weight); /*gives the value as it is a global scope variable*/
     // let keyword 
 
     let d = 'john doe';
-    // let c = 0 /*can't be redeclared */
+    // let c = 0, d = 1; /*SyntaxError: Identifier 'c' has already been declaredcan't be redeclared. c and are declared before */
+    {
+        let c = 1, d = 2; /*this is permitted as c and are declared at another block */
+        console.log(c, d)
+    }
+
+    // console.log(e); /*Uncaught ReferenceError ReferenceError: Cannot access 'e' before initialization. Like var, let declarations are also hoisted. But unlike var, let declarations without initialization are not initialized with undefined autamatically  */
+    // let e
+
+    console.log(f);
+    let f = 'fuchka' /*Uncaught ReferenceError ReferenceError: Cannot access 'f' before initialization. like before it gives same error. Because only declararions are hoisted not initialization. It means it only takes let f not let f = 'fuchka'. */
 }
